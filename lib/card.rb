8e18fd27d956355e858a9e1bfea5ec50ebd5942c
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-11-30 18:31:21 -0700

    Outline card class

---
+++ lib/card.rb
@@ -0,0 +1,22 @@
+class Card
+  attr_reader: :suit :rank :value
+
+  def initialize(suit, value, rank)
+    @suit = suit
+    @rank = rank
+    @value = value
+  end
+
+  def suit
+    @suit
+  end
+
+  def.value
+    @value
+  end
+
+  def.rank
+    @rank
+  end
+
+end
