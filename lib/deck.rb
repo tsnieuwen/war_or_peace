f2028fe0f5e9580f6a9c48197e9daa439ce3b70d
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-11-30 19:44:26 -0700

    Create 3/5 deck.rb methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,6 +3,7 @@ attr_reader :cards
 
   def initialize(cards)
     @cards = cards
+    highest_ranking_cards = []
   end
 
   def rank_of_cards_at(index)
@@ -10,9 +11,16 @@ attr_reader :cards
   end
 
   def highest_ranking_cards
+    @cards.each do |card|
+      if card.value >= 11
+        highest_ranking_cards << card
+      end
+    end
+    return highest_ranking_cards
   end
 
   def percent_high_ranking
+    return highest_ranking_cards.count / cards.count * 100
   end
 
   def remove_card
