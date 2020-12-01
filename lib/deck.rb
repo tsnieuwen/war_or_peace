88c51c17fe75aa9ed098d3146e065c764fce25d0
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-11-30 21:12:37 -0700

    Progress to percent high ranking method, deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,26 +1,33 @@
 class Deck
-attr_reader :cards
+attr_reader :cards, :high_ranking_cards
 
   def initialize(cards)
     @cards = cards
-    highest_ranking_cards = []
+    @highest_ranking_cards = []
   end
 
-  def rank_of_cards_at(index)
+  def rank_of_card_at(index)
     return @cards[index].rank
   end
 
-  def highest_ranking_cards
+  def high_ranking_cards
     @cards.each do |card|
-      if card.value >= 11
-        highest_ranking_cards << card
+      if card.rank >= 11
+        @highest_ranking_cards << card
       end
     end
-    return highest_ranking_cards
+    return @highest_ranking_cards
   end
 
   def percent_high_ranking
-    return highest_ranking_cards.count / cards.count * 100
+
+    num_high_ranking = @highest_ranking_cards.count
+    num_cards_total = @cards.count
+    num_high_ranking = num_high_ranking.to_f
+    num_cards_total = num_cards_total.to_f
+
+    return (num_high_ranking/num_cards_total)
+
   end
 
   def remove_card
