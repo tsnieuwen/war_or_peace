5862b8bc7066530a4a50a4bcbc97513944de3a29
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-01 19:05:47 -0700

    Begin writing tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,6 +1,31 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/desk'
+require './lib/deck'
 require './lib/card'
 
-class DeskTest < Minitest::Test
+class DeckTest < Minitest::Test
+
+
+  def test_it_exists
+
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_it_has_readable_cards_attributes
+
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card2, card3], deck.cards
+  end
+
+end
