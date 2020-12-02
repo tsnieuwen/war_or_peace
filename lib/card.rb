e4fe6c76b655823ce1bd12fea5c4125ac29bed85
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-01 19:04:49 -0700

    Update to only have attr_readers, no methods

--- lib/card.rb
+++ lib/card.rb
@@ -1,21 +1,12 @@
 class Card
+  attr_reader :suit,
+              :value,
+              :rank
 
   def initialize(suit, value, rank)
     @suit = suit
     @rank = rank
     @value = value
-  end
-
-  def suit
-    @suit
-  end
-
-  def value
-    @value
-  end
-
-  def rank
-    @rank
   end
 
 end
