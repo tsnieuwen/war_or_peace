02d510c0749b0fdfc7338f2b34dfe5597c0e0e13
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-02 08:09:44 -0700

    Complete draft of players and players test

--- lib/player.rb
+++ lib/player.rb
@@ -2,9 +2,18 @@ class Player
   attr_reader :name,
               :deck
 
+
   def initialize(name, deck)
     @name = name
     @deck = deck
+  end
+
+  def has_lost?
+    if deck.cards.count >= 1
+      false
+    else
+      true
+    end
   end
 
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -5,6 +5,13 @@ require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
+  def setup
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+  end
 
   def test_it_exists
     card1 = Card.new(:diamond, "Queen", 12)
@@ -33,7 +40,17 @@ class PlayerTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     player = Player.new("Ruby", deck)
-    assert_equal cards, player.deck
+    assert_equal deck, player.deck
+  end
+
+  def test_if_player_has_lost
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new("Ruby", deck)
+    assert_equal false, player.has_lost?
   end
 
 
