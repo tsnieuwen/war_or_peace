2c17a87c09573958d4b8bb2915956d3e02664171
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-03 21:15:32 -0700

    Update deck files

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,7 +6,7 @@ attr_reader :cards
   end
 
   def rank_of_card_at(index)
-    return @cards[index].rank
+    @cards[index].rank
   end
 
   def high_ranking_cards
@@ -17,39 +17,25 @@ attr_reader :cards
       end
       highest_ranking_cards = highest_ranking_cards.uniq
     end
-    return highest_ranking_cards
+    highest_ranking_cards
   end
 
   def percent_high_ranking
-    highest_ranking_cards = []
-    @cards.each do |card|
-      if card.rank >= 11
-        highest_ranking_cards << card
-      end
-    end
-    highest_ranking_cards = highest_ranking_cards.uniq
+    highest_ranking_cards = high_ranking_cards
     num_high_ranking = highest_ranking_cards.count
     num_cards_total = @cards.count
     num_high_ranking = num_high_ranking.to_f
     num_cards_total = num_cards_total.to_f
     answer = (num_high_ranking/num_cards_total) * 100.00
     answer.round(2)
-
   end
 
   def remove_card
-    highest_ranking_cards = []
-    @cards.each do |card|
-      if card.rank >= 11
-        highest_ranking_cards << card
-      end
-    end
-    highest_ranking_cards = highest_ranking_cards.uniq
+    highest_ranking_cards = high_ranking_cards
     if @cards[0].rank >= 11
       highest_ranking_cards.delete(@cards[0])
     end
     @cards.delete_at(0)
-
   end
 
   def add_card(added_card)
