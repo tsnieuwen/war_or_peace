b6dea8d292bfe4ec021a8d93288eb6550c691e2b
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-05 19:11:54 -0700

    Player_test refactor_1

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,13 +5,6 @@ require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
-  def setup
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-  end
 
   def test_it_exists
     card1 = Card.new(:diamond, "Queen", 12)
@@ -20,26 +13,19 @@ class PlayerTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     player = Player.new("Name", deck)
+
     assert_instance_of Player, player
   end
 
-  def test_if_reader_for_name
+  def test_readable_attributes
     card1 = Card.new(:diamond, "Queen", 12)
     card2 = Card.new(:spade, "3", 3)
     card3 = Card.new(:heart, "Ace", 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     player = Player.new("Ruby", deck)
-    assert_equal "Ruby", player.name
-  end
 
-  def test_if_reader_for_deck
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player = Player.new("Ruby", deck)
+    assert_equal "Ruby", player.name
     assert_equal deck, player.deck
   end
 
@@ -50,8 +36,8 @@ class PlayerTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     player = Player.new("Ruby", deck)
+
     assert_equal false, player.has_lost?
   end
-
 
 end
